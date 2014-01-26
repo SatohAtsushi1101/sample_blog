c8c6ffa920515e969145b09cf906f9f1fdcf9a96
SatohAtsushi <ar605atsushi@gmail.com>
2014-01-26 19:34:15 +0900

    controller_edit

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -20,11 +20,13 @@ SampleBlog::App.controllers :posts do
   # end
   
   get :index do
-
+    @posts = Post.all(:order => 'created_at desc')
+    render 'posts/index'
   end
 
   get :show do
-
+    @pos = Post.find_by_id(params[:id])
+    render 'posts/show'
   end
 
 end
