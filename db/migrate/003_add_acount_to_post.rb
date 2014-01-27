9ffe7f4d1a3a773d1a6751efa6f63e36f96f6c39
SatohAtsushi <ar605atsushi@gmail.com>
2014-01-27 09:21:03 +0900

    003accountPostRb

--- db/migrate/003_add_acount_to_post.rb
+++ db/migrate/003_add_acount_to_post.rb
@@ -3,11 +3,8 @@ class AddAcountToPost < ActiveRecord::Migration
     change_table :posts do |t|
       t.integer :account_id
     end
-  end
-
-  def self.down
-    change_table :posts do |t|
-      t.remove :account_id
-    end
+    # and assigns a user to all existing posts
+    first_account = Account.first
+    post.all.each { |p| p.update_attribute(:account, first_account) }
   end
 end
